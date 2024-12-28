1a8e3769e5e71ac0ac09f1ee6eda7af649fec889
sonu00000 <banik.soumajit@gmail.com>
2024-12-29 02:42:16 +0530

    react router setup with pages for paths and added mainlayout component and error element for errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,47 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import {
+  AddJob,
+  Admin,
+  AllJobs,
+  DashboardLayout,
+  DeleteJob,
+  EditJob,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Profile,
+  Register,
+  Stats,
+} from "./pages"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+])
+
 const App = () => {
-  return <h1>Job Navigator</h1>
+  return <RouterProvider router={router} />
 }
 export default App
