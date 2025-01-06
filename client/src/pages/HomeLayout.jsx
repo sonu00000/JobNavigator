6709533c48661979a4ac6e73605a20da09f68e0a
sonu00000 <banik.soumajit@gmail.com>
2025-01-06 20:22:08 +0530

    landing page

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -2,10 +2,9 @@ import { Outlet } from "react-router-dom"
 
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>Navbar</nav>
+    <>
       <Outlet />
-    </div>
+    </>
   )
 }
 export default HomeLayout
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,39 @@
+import styled from "styled-components"
+import Wrapper from "../assets/wrappers/LandingPage"
+import main from "../assets/images/main.svg"
+import { Link } from "react-router-dom"
+import Logo from "../components/Logo"
+
 const Landing = () => {
-  return <div>Landing</div>
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            JobNavigator is your ultimate companion for discovering career
+            opportunities tailored to your skills and aspirations. Whether
+            you're just starting out or looking for your next big move, our app
+            connects you with the right roles, companies, and resources to help
+            you succeed. Take control of your journey and navigate your way to
+            the perfect job with ease and confidence!
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  )
 }
+
 export default Landing
